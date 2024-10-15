750eb85f97182aa812d1e98a10e49cd1d82ec715
Victor Mora <victor.mora.4b@gmail.com>
2024-10-15 18:52:06 +0800

    First classes express

---
+++ 05Express/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("In the middleware");
+  next();
+});
+app.use((req, res, next) => {
+  res.send("<h1>HELLO!</h1>");
+});
+
+app.listen(3000)
